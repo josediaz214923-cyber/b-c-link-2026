0c11b729978c31ed16990d1c1e651514b88b43ad
josediaz214923-cyber <josediaz214923@gmail.com>
2026-04-02 00:33:45 +0700

    Update rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -123,7 +123,7 @@ export default defineConfig({
     ],
     html: {
         favicon: './src/assets/icon.ico',
-        title: 'Facebook',
+        title: 'tuiday',
         meta: {
             viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
             'og:image':
